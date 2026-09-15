c6602a8a9004b54fe4bd688bbd1138c826a3e283
gabriel <gacamargo8@gmail.com>
2026-09-15 12:53:05 -0300

    limpa valor nulo e configura banco de dados
    Tratamentos dos valores nulos, arruma valor de data e configura a conexão com postgres

--- pipeline.py
+++ pipeline.py
@@ -1,13 +1,30 @@
 import pandas as pd
 import numpy as np
+import os
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
+#configuração do banco de dados
+load_dotenv()
+
+usuario = os.getenv('DB_USER')
+senha = os.getenv('DB_PASSWORD')
+host = os.getenv('DB_HOST')
+porta = os.getenv('DB_PORT')
+banco = os.getenv('DB_NAME')
+
+engine = create_engine(f'postgresql+psycopg2://{usuario}:{senha}@{host}:{porta}/{banco}')
+
+#importando tabela
 df = pd.read_csv('vendas_livrarias.csv')
 
-#variavel utilizada para checar duplicatas
-#tp = pd.concat((df['livro'].duplicated().rename('duplicata'), df['livro']), axis=1)
-
 #arrumando valores nulos
-df[]=
-
+df['filial'] = df['filial'].fillna(value = 'Não Informado')
+df['categoria'] = df['categoria'].fillna(value = 'Não Informado')
+df = df.dropna(subset = 'quantidade_vendida')
+df = df.dropna(subset = 'preco_unitario')
+df = df.dropna(subset = 'custo_unitario')
+df = df.dropna(subset = 'data_venda')
 
 #padronizando os valores
 remocao = str.maketrans(',', '.', 'R$')
@@ -15,7 +32,7 @@ df['preco_unitario'] = df['preco_unitario'].str.translate(remocao)
 df['custo_unitario'] = df['custo_unitario'].str.translate(remocao)
 df['preco_unitario'] = df['preco_unitario'].astype(float)
 df['custo_unitario'] = df['custo_unitario'].astype(float)
-df['data_venda'] = pd.to_datetime(df['data_venda'])
+df['data_venda'] = pd.to_datetime(df['data_venda'], dayfirst = True)
 df = df.drop_duplicates(subset = ['livro'], keep = 'first')
 
 #enriquecendo os dados
@@ -23,7 +40,6 @@ df['receita_total'] = df['quantidade_vendida'] * df['preco_unitario']
 df['margem_lucro'] = (df['custo_unitario'] - df['preco_unitario']) * df['quantidade_vendida']
 df['alerta_prejuizo'] = np.where(df['margem_lucro'] > 0, True, False) 
 
+#exportando tabela com filtro
+df[df['quantidade_vendida']>5].to_sql(name='vendas', con = engine, if_exists = 'append', index = False)
 
-#df[df['quantidade_vendas']>5]
-
-print(df)
